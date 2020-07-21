3a06c0b601e9aed04c8e7699692e482caaab4906
GMasterHD <craftmechanicsgame@gmail.com>
2020-07-21 20:35:14 +0200

    Added premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+project "JSONCpp"
+    kind "StaticLib"
+    language "C"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/include/**.hpp",
+        "%{prj.name}/src/**.c",
+        "%{prj.name}/include/**.h"
+    }
+
+    includedirs {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
